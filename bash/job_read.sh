165b517e934e9e3137a3d3367657e3bb9e0f3080
rofeld <rofeld.mailbox@gmail.com>
2020-05-04 23:16:27 -0400

    added restart from point of failure functionality

--- bash/job_read.sh
+++ bash/job_read.sh
@@ -5,16 +5,42 @@ source ./jobber_func.sh
 input="job.flow"
 count=0
 counter=0
-while read -r line
+current_file=`basename "$0"`
+current_file="${current_file}_tmp"
+restart=""
+
+cp "${input}" "${input}_tmp"
+input_tmp="${input}_tmp"
+
+
+if [ -e ${current_file} ]; then
+  restart=`cat ${current_file}`
+  sed -n '/'"${restart}"'/,$p' "${input_tmp}" > "${input_tmp}2"
+  mv "${input_tmp}2" "${input_tmp}"
+fi
+
+WAIT_ERROR=0
+
+while read -r line && [[ "$WAIT_ERROR" -eq 0 ]]
 do
   #echo "line: $line"
 
-  if echo "${line}" | grep -q "="; then
-    count=`echo "${line}" | awk -F'=' '{print $2}'`
+  if echo "${line}" | grep -q "cmd="; then
+    #count=`echo "${line}" | awk -F'=' '{print $2}'`
     #echo "count: $count"
+    count=0
     if [ "$counter" -gt 0 ]; then
-        wait
+      for pid in ${pids[*]}; do
+        wait $pid || let "WAIT_ERROR+=1"
+      done
     fi
+
+    if [ "$WAIT_ERROR" -gt 0 ]; then
+      break
+    fi
+
+    label=${line}
+    echo "${label}" > ${current_file}
 
 #    if [ "$count" -eq 1 ]; then
 #      echo "ONE"
@@ -23,11 +49,32 @@ do
 #    fi
 
   else
+    if [ "$WAIT_ERROR" -gt 0 ]; then
+      break
+    fi
     echo "executing... job_exec ${line} &"
-    job_exec ${line} &
+      let "count+=1"
+      job_exec ${line} &
+      pids[${count}]=$!
 
   fi
   let "counter=counter+1"
-done < "$input"
 
-wait
+  if [ "$WAIT_ERROR" -gt 0 ]; then
+    break
+  fi
+done < "$input_tmp"
+
+if [ "$WAIT_ERROR" -gt 0 ]; then
+  break
+fi
+
+for pid in ${pids[*]}; do
+    wait $pid || let "WAIT_ERROR+=1"
+done
+
+if [ "$WAIT_ERROR" -gt 0 ]; then
+  exit 1
+else
+  rm -if ${current_file}
+fi
